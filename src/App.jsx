ff70b1c4e3cc8d2154a9558a738716fb36e60b5f
bhaskarvb28 <bhaskarvb28@gmail.com>
2026-02-05 19:58:19 +0530

    chore: init react dev base

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>MyApp – DEV</h1>
+      <p>React dev environment is running ✅</p>
+    </div>
+  );
+}
+
+export default App;
