bb8beeb3c23af055eea6095f22434851a1db8832
bhaskarvb28 <bhaskarvb28@gmail.com>
2026-02-05 20:19:11 +0530

    feat: read notes from dev database

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
+import { useEffect, useState } from "react";
+import { supabase } from "./supabase";
 import './App.css'
-
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    async function load() {
+      const { data, error } = await supabase
+        .from("notes")
+        .select("*");
+
+      if (!error) setNotes(data);
+    }
+
+    load();
+  }, []);
+
   return (
     <div style={{ padding: 40 }}>
       <h1>MyApp – DEV</h1>
       <p>React dev environment is running ✅</p>
+
+
+      <ul>
+        {notes.map((n) => (
+          <li key={n.id}>{n.content}</li>
+        ))}
+      </ul>
     </div>
   );
 }
