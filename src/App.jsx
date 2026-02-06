88132777099ba1d14999a269a51bed20b5b530ce
bhaskarvb28 <bhaskarvb28@gmail.com>
2026-02-06 15:58:11 +0530

    feature/login

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
   return (
     <div style={{ padding: 40 }}>
       <h1>MyApp – DEV</h1>
-      <p>React dev environment is running ✅</p>
+      <p>React dev ✅</p>
 
 
       <ul>
